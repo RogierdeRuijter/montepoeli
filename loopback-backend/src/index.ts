f79fcc5586e2dd84750ea78070a24bfaa43845a0
Rogier de Ruijter <rogierderuijter@hotmail.com>
2018-11-12 10:43:46 +0100

    initial commit

---
+++ loopback-backend/src/index.ts
@@ -0,0 +1,16 @@
+import {MontepouliBackend} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {MontepouliBackend};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new MontepouliBackend(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
