0ae281bfcb6a34dd84f996aa24f79bad20a393ea
RogierDeRuijter <rogierderuijter@hotmail.com>
2018-12-10 13:25:09 +0100

    Serve static files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import {MontepouliBackend} from './application';
 import {ApplicationConfig} from '@loopback/core';
+import * as path from 'path';
 
 export {MontepouliBackend};
 
@@ -9,6 +10,9 @@ export async function main(options: ApplicationConfig = {}) {
   await app.start();
 
   const url = app.restServer.url;
+
+  app.static('/', path.join(__dirname, '../client/dist'));
+
   console.log(`Server is running at ${url}`);
   console.log(`Try ${url}/ping`);
 
