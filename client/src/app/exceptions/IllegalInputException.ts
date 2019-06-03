472f10dda29a2f80088d81eed3112fad9eb7f821
RogierDeRuijter <rogierderuijter@hotmail.com>
2019-06-03 21:23:04 +0200

    Fix wrong prototype

--- client/src/app/exceptions/IllegalInputException.ts
+++ client/src/app/exceptions/IllegalInputException.ts
@@ -1,10 +1,8 @@
-import {UnknownCaseException} from './UnknownCaseException';
-
 export class IllegalInputException extends Error {
   constructor(message: string) {
     super(message);
 
-    Object.setPrototypeOf(this, UnknownCaseException.prototype);
+    Object.setPrototypeOf(this, IllegalInputException.prototype);
   }
 
 }
