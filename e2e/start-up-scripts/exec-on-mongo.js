1b329be658c1717106b440b0a265d76ba95f7809
RogierDeRuijter <rogierderuijter@hotmail.com>
2019-09-02 14:07:48 +0200

    Fix deprecation warning

--- e2e/start-up-scripts/exec-on-mongo.js
+++ e2e/start-up-scripts/exec-on-mongo.js
@@ -3,7 +3,10 @@ const MongoClient = require('mongodb').MongoClient;
 module.exports = (dbParams, fn) => {
   let client;
   return MongoClient
-    .connect(dbParams.url)
+    .connect(dbParams.url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then((iClient) => {
       client = iClient;
       return client.db(dbParams.dbName);
