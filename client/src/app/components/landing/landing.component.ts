734af0433e0634b513ed996b63ea55d8b1a9f68e
RogierdeRuijter <rogierderuijter@hotmail.com>
2019-11-19 20:29:04 +0100

    Feature/landing page (#272)
    * Add landing component and use environment configured routes
    
    * Fix routes
    
    * Attempt to fix end-2-end tests
    
    * Fix
    
    * fix e2e tests

--- client/src/app/components/landing/landing.component.ts
+++ client/src/app/components/landing/landing.component.ts
@@ -1,10 +1,24 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AuthService} from '../../services/auth/auth.service';
+import {Environment} from '../../../environments/environment';
 
 @Component({
-  selector: 'app-landing',
-  templateUrl: './landing.component.html',
-  styleUrls: ['landing.component.scss']
+  template: ``
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
 
+  private environment = new Environment();
+
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute,
+              private authService: AuthService) { }
+
+  public ngOnInit(): void {
+    if (this.authService.isAuthenticated()) {
+      this.router.navigate(['../' + this.environment.frontend.BASIC_ROUTES.HOME], {relativeTo: this.activatedRoute});
+    } else {
+      this.router.navigate(['../' + this.environment.frontend.BASIC_ROUTES.LOGIN], {relativeTo: this.activatedRoute});
+    }
+  }
 }
