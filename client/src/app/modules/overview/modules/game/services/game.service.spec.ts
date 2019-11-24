2190574cd7e7b9b9c8e869d87dc44fa6c2d5fc02
RogierDeRuijter <rogierderuijter@hotmail.com>
2019-11-24 14:09:05 +0100

    fix unit test

--- client/src/app/modules/overview/modules/game/services/game.service.spec.ts
+++ client/src/app/modules/overview/modules/game/services/game.service.spec.ts
@@ -3,12 +3,15 @@ import {TestBed} from '@angular/core/testing';
 import {GameService} from './game.service';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {EnvironmentService} from '../../../../shared/services/environment/environment.service';
+import { CookieService } from 'ngx-cookie-service';
+import { MockLocationStrategy } from '@angular/common/testing';
 
 describe('GameService', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [HttpClientTestingModule],
     providers: [
-      EnvironmentService
+      EnvironmentService,
+      {provide: CookieService, useClass: MockLocationStrategy}
     ]
   }));
 
