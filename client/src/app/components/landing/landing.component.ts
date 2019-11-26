8ac6f622fc6bc00f4fa8bfa46f50d69b1231fee7
rogier <rogier.de.ruijter-cic.netherlands@ibm.com>
2019-11-26 17:31:32 +0100

    Fix merge

--- client/src/app/components/landing/landing.component.ts
+++ client/src/app/components/landing/landing.component.ts
@@ -1,8 +1,26 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {Environment} from '../../../environments/environment';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AuthService} from '../../services/auth/auth.service';
 
 @Component({
   template: ``,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
+
+  private environment = new Environment();
+
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute,
+              private authService: AuthService) {
+  }
+
+  public ngOnInit(): void {
+    if (this.authService.isAuthenticated()) {
+      this.router.navigate(['../' + this.environment.frontend.BASIC_ROUTES.HOME], {relativeTo: this.activatedRoute});
+    } else {
+      this.router.navigate(['../' + this.environment.frontend.BASIC_ROUTES.LOGIN], {relativeTo: this.activatedRoute});
+    }
+  }
 }
