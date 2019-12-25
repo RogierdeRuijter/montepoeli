2d7d864ef324ab8c05dbeceb576c9bef3b6048e3
RogierDeRuijter <rogierderuijter@hotmail.com>
2019-12-25 16:34:35 +0100

    better split mergify

--- .mergify/lint-mergify.sh
+++ .mergify/lint-mergify.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-RESPONSE=$(curl -s -F 'data=@.mergify.yml' https://gh.mergify.io/validate)
+RESPONSE=$(curl -s -F 'data=@config.yml' https://gh.mergify.io/validate)
 echo $RESPONSE
 if [ "${RESPONSE}" != "The configuration is valid" ]; then
     exit 125
