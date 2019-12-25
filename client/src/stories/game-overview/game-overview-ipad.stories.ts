e06c523e384b3a1ac57251c398b22bed61543a39
RogierDeRuijter <rogierderuijter@hotmail.com>
2019-12-25 22:51:32 +0100

    remove storybook build files and more

--- client/src/stories/game-overview/game-overview-ipad.stories.ts
+++ client/src/stories/game-overview/game-overview-ipad.stories.ts
@@ -2,6 +2,6 @@ import {storiesOf} from '@storybook/angular';
 import {GameOverviewStoryFactory} from '../helpers/factories/game-overview.story-factory';
 
 const storyDefinition = storiesOf('OverviewComponent/iPad', module)
-  .addParameters({ viewport: { defaultViewport: 'tablet' }})
+  // .addParameters({ viewport: { defaultViewport: 'tablet' }})
 
 new GameOverviewStoryFactory(storyDefinition).create();
