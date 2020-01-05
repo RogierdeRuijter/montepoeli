77e93f9ea60290163cceb16e9d35e198fc037f97
RogierDeRuijter <rogierderuijter@hotmail.com>
2020-01-05 17:27:45 +0100

    add y

--- .circleci/bump-version.sh
+++ .circleci/bump-version.sh
@@ -1,13 +1,13 @@
 BRANCH_NAME=maintenance/
 
 if [[ $BRANCH_NAME == *"maintenance"* ]]; then
-  lerna version patch
+  lerna version -y patch
 fi
 
 if [[ $BRANCH_NAME == *"feature"* ]]; then
-  lerna version minor
+  lerna version -y minor
 fi
 
 if [[ $BRANCH_NAME == *"major-feature"* ]]; then
-  lerna version major
+  lerna version -y major
 fi
