318e2ef80b25f7c88c9947e36b40694dff8e47bb
RogierDeRuijter <rogierderuijter@hotmail.com>
2020-01-05 17:48:52 +0100

    bump version in pipeline

--- .circleci/bump-version.sh
+++ .circleci/bump-version.sh
@@ -1,13 +1,13 @@
-BRANCH_NAME=maintenance/
+BRANCH_NAME=feature
 
 if [[ $BRANCH_NAME == *"maintenance"* ]]; then
-  lerna version -y patch
+  lerna version -y patch --tag-version-prefix [ci skip]
 fi
 
 if [[ $BRANCH_NAME == *"feature"* ]]; then
-  lerna version -y minor
+  lerna version -y minor --tag-version-prefix [ci skip]
 fi
 
 if [[ $BRANCH_NAME == *"major-feature"* ]]; then
-  lerna version -y major
+  lerna version -y major --tag-version-prefix [ci skip]
 fi
