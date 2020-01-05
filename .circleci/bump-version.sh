7258eda2d0ca6b666f38c4355a7d042fd76d63f2
RogierDeRuijter <rogierderuijter@hotmail.com>
2020-01-05 18:15:58 +0100

    fix script and try it as the first command

--- .circleci/bump-version.sh
+++ .circleci/bump-version.sh
@@ -1,13 +1,13 @@
 BRANCH_NAME=feature
 
 if [[ $BRANCH_NAME == *"maintenance"* ]]; then
-  lerna version -y patch --tag-version-prefix [ci skip]
+  lerna version -y patch --message '[ci skip] bump patch'
 fi
 
 if [[ $BRANCH_NAME == *"feature"* ]]; then
-  lerna version -y minor --tag-version-prefix [ci skip]
+  lerna version -y minor --message '[ci skip] bump minor'
 fi
 
 if [[ $BRANCH_NAME == *"major-feature"* ]]; then
-  lerna version -y major --tag-version-prefix [ci skip]
+  lerna version -y major --message '[ci skip] bump major'
 fi
