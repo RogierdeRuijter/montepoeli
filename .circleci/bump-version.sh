332e326e66b4bfb0d822381eaa3aee5ec9eae013
RogierDeRuijter <rogierderuijter@hotmail.com>
2020-01-05 18:07:16 +0100

    test1

--- .circleci/bump-version.sh
+++ .circleci/bump-version.sh
@@ -1,13 +1,13 @@
 BRANCH_NAME=feature
 
 if [[ $BRANCH_NAME == *"maintenance"* ]]; then
-  lerna version -y patch --tag-version-prefix [ci skip]
+  lerna version -y patch --tag-version-prefix '[ci skip]'
 fi
 
 if [[ $BRANCH_NAME == *"feature"* ]]; then
-  lerna version -y minor --tag-version-prefix [ci skip]
+  lerna version -y minor --tag-version-prefix '[ci skip]'
 fi
 
 if [[ $BRANCH_NAME == *"major-feature"* ]]; then
-  lerna version -y major --tag-version-prefix [ci skip]
+  lerna version -y major --tag-version-prefix '[ci skip]'
 fi
