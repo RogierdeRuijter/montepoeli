ad7f6d47d1ffcc26c14bcd312553724f9a24f272
RogierDeRuijter <rogierderuijter@hotmail.com>
2020-01-05 21:52:14 +0100

    move to yarn command

--- .circleci/bump-version.sh
+++ .circleci/bump-version.sh
@@ -1,13 +1,13 @@
 CIRCLE_BRANCH=feature
 
 if [[ $CIRCLE_BRANCH == *"maintenance"* ]]; then
-  lerna version -y patch --message '[ci skip] bump patch'
+  yarn release-patch
 fi
 
 if [[ $CIRCLE_BRANCH == *"feature"* ]]; then
-  lerna version -y minor --message '[ci skip] bump minor'
+  yarn release-minor
 fi
 
 if [[ $CIRCLE_BRANCH == *"major-feature"* ]]; then
-  lerna version -y major --message '[ci skip] bump major'
+  yarn release-major
 fi
