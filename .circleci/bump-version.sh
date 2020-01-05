be681550e92b2590b5a97ff5165d1cae574a0684
RogierDeRuijter <rogierderuijter@hotmail.com>
2020-01-05 18:19:02 +0100

    fix env variable in script

--- .circleci/bump-version.sh
+++ .circleci/bump-version.sh
@@ -1,13 +1,13 @@
-BRANCH_NAME=feature
+CIRCLE_BRANCH=feature
 
-if [[ $BRANCH_NAME == *"maintenance"* ]]; then
+if [[ $CIRCLE_BRANCH == *"maintenance"* ]]; then
   lerna version -y patch --message '[ci skip] bump patch'
 fi
 
-if [[ $BRANCH_NAME == *"feature"* ]]; then
+if [[ $CIRCLE_BRANCH == *"feature"* ]]; then
   lerna version -y minor --message '[ci skip] bump minor'
 fi
 
-if [[ $BRANCH_NAME == *"major-feature"* ]]; then
+if [[ $CIRCLE_BRANCH == *"major-feature"* ]]; then
   lerna version -y major --message '[ci skip] bump major'
 fi
