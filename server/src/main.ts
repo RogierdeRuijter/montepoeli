78e38aa496672373295f32ebbae1c878c629047a
Rogier de Ruijter <rogier.de.ruijter-CIC.Netherlands@ibm.com>
2020-01-26 10:20:25 +0100

    feat: gzip compression for nodejs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './modules/app/app.module';
 import * as cookieParser from 'cookie-parser';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,7 @@ async function bootstrap() {
   checkEnvironmentVariables();
 
   app.use(cookieParser(process.env.COOKIE_SECRET));
+  app.use(compression({threshold: 500}));
   app.setGlobalPrefix('api');
 
   let origin: boolean | string = true;
