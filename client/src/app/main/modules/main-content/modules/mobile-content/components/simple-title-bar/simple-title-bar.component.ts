c4ea11b0a49ca405cb9e04c48b6623a55f9119f4
Rogier De Ruijter <rogierderuijter@hotmail.com>
2020-03-21 12:34:21 +0100

    feat: add fade in anmiation to game table and montepoeli logo

--- client/src/app/main/modules/main-content/modules/mobile-content/components/simple-title-bar/simple-title-bar.component.ts
+++ client/src/app/main/modules/main-content/modules/mobile-content/components/simple-title-bar/simple-title-bar.component.ts
@@ -1,12 +1,22 @@
 import { ChangeDetectionStrategy, Component, Output, EventEmitter } from '@angular/core';
 import { Icons, IconSize } from '../../../../../../../shared/static-files/enums';
+import { trigger, transition, query, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-simple-title-bar',
   templateUrl: './simple-title-bar.component.html',
   styleUrls: ['./simple-title-bar.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('fadeInAnimation', [
+    transition('* => *', [
+    query(':enter', style({ opacity: 0 })),
+    query(':enter', animate('600ms', style({ opacity: 1 })))
+    ])
+  ])
+  ]
 })
+
 export class SimpleTitleBarComponent {
   @Output()
   public iconClicked: EventEmitter<void> = new EventEmitter();
