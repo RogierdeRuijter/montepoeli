1543d761daff94a968cb19b487068e43dfec1179
Rogier De Ruijter <rogierderuijter@hotmail.com>
2020-03-21 12:34:21 +0100

    feat: add fade in anmiation to game table and montepoeli logo

--- client/src/app/shared/modules/home/modules/game/components/overview/game-table/game-table.component.ts
+++ client/src/app/shared/modules/home/modules/game/components/overview/game-table/game-table.component.ts
@@ -1,11 +1,20 @@
 import {Component, Input} from '@angular/core';
 import {Game} from '../../../../../../../../shared/interfaces/game.interface';
 import {Observable} from 'rxjs';
+import { trigger, transition, query, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'ui-game-table',
   templateUrl: './game-table.component.html',
   styleUrls: ['./game-table.component.scss'],
+  animations: [
+    trigger('rowAnimation', [
+    transition('* => *', [
+    query(':enter', style({ opacity: 0 })),
+    query(':enter', animate('600ms', style({ opacity: 1 })))
+  ])
+])
+  ]
 })
 export class GameTableComponent {
 
