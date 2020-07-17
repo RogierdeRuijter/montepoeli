1f7bdaf8ece8399c996fccdc846bba9317f04fbe
Rogier De Ruijter <rogierderuijter@hotmail.com>
2020-07-17 12:38:42 +0200

    add some stuff

--- apps/api/src/modules/sync-game/sync-game.gateway.ts
+++ apps/api/src/modules/sync-game/sync-game.gateway.ts
@@ -1,15 +1,20 @@
 import { SubscribeMessage, WebSocketGateway, OnGatewayConnection, WebSocketServer } from '@nestjs/websockets';
 import { Socket } from 'dgram';
+import { GameService } from '../game/game.service/game.service';
 
 @WebSocketGateway()
 export class SyncGameGateway implements OnGatewayConnection {
 
+  constructor(private gameService: GameService) {}
+
   @WebSocketServer()
   private server;
 
-  public handleConnection(client: Socket, ...args: any[]) {
+  public async handleConnection(client: Socket, ...args: any[]) {
+    const gameIds: string[] = await this.gameService.getAllIdsFromGames();
     // TODO: emit all the game ids to the frontend
-    // this.server.emit('users', this.users);
+    // this.server.emit('gameIds', gameIds);
+    // console.log('in on gateway connection');
   }
 
   @SubscribeMessage('message')
