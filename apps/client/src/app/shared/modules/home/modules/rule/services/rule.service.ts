7928c3f02bb4ee143f62b8fe6a6a6aaedfa3196a
Rogier De Ruijter <rogierderuijter@hotmail.com>
2020-11-14 15:37:45 +0100

    remove any interface for environment

--- apps/client/src/app/shared/modules/home/modules/rule/services/rule.service.ts
+++ apps/client/src/app/shared/modules/home/modules/rule/services/rule.service.ts
@@ -3,18 +3,19 @@ import { CustomHttpService } from '../../../../../../shared/modules/http/service
 import { Observable } from 'rxjs';
 import { Rule } from '../../../../../../shared/interfaces/rule.interface';
 import { EnvironmentService } from '../../../../../../shared/services/environment/environment.service';
+import { Environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RuleService {
-  public environment: any;
+  public environment: Environment;
 
   constructor(private httpService: CustomHttpService, private environmentService: EnvironmentService) {
     this.environment = this.environmentService.get();
   }
 
   public getAll(): Observable<Rule[]> {
-    return this.httpService.get<Rule[]>(this.environment.backend.ENTRY_POINTS.RULE);
+    return this.httpService.get<Rule[]>(this.environment.backend.entryPoints.rule);
   }
 }
