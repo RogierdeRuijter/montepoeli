af606767fb8c5271feb71bbf1f2ee3e59e97851c
Rogier De Ruijter <rogierderuijter@hotmail.com>
2021-01-03 20:22:44 +0100

    create refresh token method

--- apps/api/src/modules/refresh-token/refresh-token-repository/refresh-token-repository.service.ts
+++ apps/api/src/modules/refresh-token/refresh-token-repository/refresh-token-repository.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { RefreshToken } from '../refresh-token.model';
+import { Model } from 'mongoose';
+import { User } from 'apps/api/src/models/interfaces/user.interface';
 
 @Injectable()
-export class RefreshTokenRepositoryService {}
+export class RefreshTokenRepositoryService {
+  constructor(@InjectModel(RefreshToken.name) private readonly refreshTokenModel: Model<any>) {}
+
+  public async createRefreshToken(user: User, timeToLive: number): Promise<RefreshToken> {
+    const token = new RefreshToken();
+
+    token.userId = user.id;
+    token.isRevoked = false;
+
+    const expiration = new Date();
+    expiration.setTime(expiration.getTime() + timeToLive);
+
+    token.expires = expiration;
+
+    return new this.refreshTokenModel(token).save();
+  }
+}
