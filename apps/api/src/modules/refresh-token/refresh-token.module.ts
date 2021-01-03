d0f807d3ab8bc11ba2b4ef4ef45aeb45c3241fd5
Rogier De Ruijter <rogierderuijter@hotmail.com>
2021-01-03 20:03:19 +0100

    add refresh token to mongoose module

--- apps/api/src/modules/refresh-token/refresh-token.module.ts
+++ apps/api/src/modules/refresh-token/refresh-token.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { RefreshToken, RefreshTokenSchema } from './refresh-token.model';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+@Module({
+  imports: [MongooseModule.forFeature([{ name: RefreshToken.name, schema: RefreshTokenSchema }])],
+})
 export class RefreshTokenModule {}
