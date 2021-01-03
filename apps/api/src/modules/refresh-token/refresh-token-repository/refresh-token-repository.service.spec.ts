c0184ca4c1f620ed5272743420c201124d4cbfda
Rogier De Ruijter <rogierderuijter@hotmail.com>
2021-01-03 20:25:54 +0100

    add get model token provider to repository unit test

--- apps/api/src/modules/refresh-token/refresh-token-repository/refresh-token-repository.service.spec.ts
+++ apps/api/src/modules/refresh-token/refresh-token-repository/refresh-token-repository.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RefreshTokenRepositoryService } from './refresh-token-repository.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { RefreshToken } from '../refresh-token.model';
 
 describe('RefreshTokenRepositoryService', () => {
   let service: RefreshTokenRepositoryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RefreshTokenRepositoryService],
+      providers: [
+        RefreshTokenRepositoryService,
+        {
+          provide: getModelToken(RefreshToken.name),
+          useValue: '',
+        },
+      ],
     }).compile();
 
     service = module.get<RefreshTokenRepositoryService>(RefreshTokenRepositoryService);
